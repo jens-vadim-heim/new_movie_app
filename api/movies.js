ddabf21404117f6b357da87958b8aa1e8dbe5565
Jens Vadim Heim <jensvadim@hotmail.com>
2025-10-06 16:52:19 +0200

    add try catch in handler function

--- api/movies.js
+++ api/movies.js
@@ -1,14 +1,20 @@
 export default async function handler(req, res) {
-  const { query } = req.query;
-  console.log(query);
   try {
+    const { query } = req.query;
+    if (!query) return res.status(400).json({ error: "Missing query" });
+
     const response = await fetch(
       `https://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&s=${query}`
     );
+
+    if (!response.ok) {
+      return res.status(response.status).json({ error: "OMDB fetch failed" });
+    }
+
     const data = await response.json();
     res.status(200).json(data);
-  } catch (error) {
-    console.error("Error occurred");
-    json.status(400).json({ error });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
   }
 }
